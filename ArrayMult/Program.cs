b3ce29acbd10bba9da7ed4c8be2fb34a56ff0c28
Clara Antunes <claraantunes.work@gmail.com>
2024-03-06 18:12:45 +0000

    Add Convert arguments to float

--- ArrayMult/Program.cs
+++ ArrayMult/Program.cs
@@ -6,7 +6,18 @@ namespace ArrayMult
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            float[,] A = new float[2, 2];
+            float[] b = new float[2];
+
+            A = new float[2, 2]
+            {
+                { float.Parse(args[0]), float.Parse(args[1]) },
+                { float.Parse(args[2]), float.Parse(args[3]) }
+            };
+
+            b[0] = float.Parse(args[4]);
+            b[1] = float.Parse(args[5]);
+            
         }
     }
 }

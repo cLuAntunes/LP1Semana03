2669e1a051327ea54b90a45ecaccd7151f58ce01
Clara Antunes <claraantunes.work@gmail.com>
2024-03-07 17:43:43 +0000

    Undid Modifications

--- PowTwoUntil/Program.cs
+++ PowTwoUntil/Program.cs
@@ -6,21 +6,15 @@ namespace PowTwoUntil
     {
         private static void PowersOf2Until5()
         {
-            PowersOf2UntilN(5);
-        }
-
-        private static void PowersOf2UntilN(int n)
-        {
-            for (int i = 1; i <= (1 << n); i = i << 1)
+            for (int i = 1; i <= (1 << 5); i = i << 1)
             {
                 Console.WriteLine(i);
             }
         }
-
+        
         static void Main(string[] args)
         {
-            PowersOf2UntilN(8);
-            PowersOf2UntilN(3);
+            PowersOf2Until5();
             PowersOf2Until5();
 
         }

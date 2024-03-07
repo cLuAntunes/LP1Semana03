c4776d45b52a2da2aa4252b59fb1d327ed7d1581
Clara Antunes <claraantunes.work@gmail.com>
2024-03-07 17:30:55 +0000

    Added PowersOf2UntilN Method and Invoked it twice from Main

--- PowTwoUntil/Program.cs
+++ PowTwoUntil/Program.cs
@@ -11,10 +11,19 @@ namespace PowTwoUntil
                 Console.WriteLine(i);
             }
         }
+
+        private static void PowersOf2UntilN(int n)
+        {
+            for (int i = 1; i <= (1 << n); i = i << 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
         static void Main(string[] args)
         {
-            PowersOf2Until5();
-            PowersOf2Until5();
+            PowersOf2UntilN(8);
+            PowersOf2UntilN(3);
 
         }
     }
